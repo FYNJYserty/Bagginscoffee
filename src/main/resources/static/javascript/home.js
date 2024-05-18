084909ca746adbd6e75f37beb4930a572a7ec007
FYNJYserty <flipper02003@gmail.com>
2024-05-18 15:30:40 +0300

    Added new changes

--- src/main/resources/static/javascript/home.js
+++ src/main/resources/static/javascript/home.js
@@ -1,4 +1,10 @@
-function changeTheme() {
-    const body = document.body;
+function toggleTheme() {
+    var body = document.forms;
+    var themeIcon = document.querySelector('.theme-icon');
     body.classList.toggle('dark-theme');
+    if (body.classList.contains('dark-theme')) {
+        themeIcon.src = 'dark-theme-icon.png';
+    } else {
+        themeIcon.src = 'light-theme-icon.png';
+    }
 }
